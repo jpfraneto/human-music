2d2f4a64dbadf228736fa8625f501211cee2aad3
jpfraneto <jpfraneto@gmail.com>
2020-11-07 13:21:40 -0300

    activated the timewarp

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ if(process.env.NODE_ENV === 'production') {
 // setTimeout(chiita.createNewDay, 2000);
 
 //If the app crashes, or the dynos are being cycled, this function will update the system and have it working nice.
-// console.log("The app.js file is running again.");
-// chiita.timeWarp();
+console.log("The app.js file is running again.");
+chiita.timeWarp();
 
 setInterval(()=>{
     console.log("This message is logged every 15 minutes")
