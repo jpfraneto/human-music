e467e6367de2ec56aca96b0e49e27b114982fd38
jpfraneto <jpfraneto@gmail.com>
2020-12-04 11:12:47 -0300

    hola

--- public/scripts/endOfDayScript.js
+++ public/scripts/endOfDayScript.js
@@ -2,20 +2,9 @@ let nextDayStartingTimestamp;
 let thisDayStartingTimestamp;
 
 window.onload = () => {
-  systemInformationPromise = getSystemInformation();
-  systemInformationPromise.then((systemInformation) => {
-    let now = (new Date).getTime();
-    delay = systemInformation.dayStartTimestamp + 86400000 - now;
-    setTimeout (() => {
-      window.location.reload()
-    }, delay);
-  })
-}
-
-async function getSystemInformation () {
-  const response = await fetch("/checkSystemStatus");
-  const presentStatus = await response.json();
-  return presentStatus
+  setTimeout (() => {
+    window.location.reload()
+  }, 5000);
 }
   
 function updateClock () {
